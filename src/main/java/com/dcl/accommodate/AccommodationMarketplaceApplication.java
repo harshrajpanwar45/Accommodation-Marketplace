ad4e193f875fd81d34ea10f410a488dce48d0025
rajugowda1806 <raju@dheecodinglab.com>
2025-10-04 09:07:19 +0530

    project initiated

---
+++ src/main/java/com/dcl/accommodate/AccommodationMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.dcl.accommodate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcl/accommodate/AccommodationMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcl.accommodate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
