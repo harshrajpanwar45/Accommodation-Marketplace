c58b1902f1ef287c5837289ec3da20ce2990551d
harshrajpanwar45 <harshrajpanwar45@gmail.com>
2025-10-06 11:53:10 +0530

    user entity setup

---
+++ src/main/java/com/dcl/accommodate/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dcl.accommodate.repository;
+
+import com.dcl.accommodate.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
