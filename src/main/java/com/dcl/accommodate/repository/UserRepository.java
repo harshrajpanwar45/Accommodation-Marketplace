57f5b85d0114237c1ef91e38901d52208ac1bcbd
harshrajpanwar45 <harshrajpanwar45@gmail.com>
2025-10-09 02:22:18 +0530

    done

--- src/main/java/com/dcl/accommodate/repository/UserRepository.java
+++ src/main/java/com/dcl/accommodate/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package com.dcl.accommodate.repository;
 
 import com.dcl.accommodate.model.User;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
