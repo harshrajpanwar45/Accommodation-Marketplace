8d8004a0e3a3ce4ebb153c06703abf881a535f63
harshrajpanwar45 <harshrajpanwar45@gmail.com>
2025-10-08 12:12:06 +0530

    token generation logic

--- src/main/java/com/dcl/accommodate/AccommodationMarketplaceApplication.java
+++ src/main/java/com/dcl/accommodate/AccommodationMarketplaceApplication.java
@@ -1,16 +1,18 @@
 package com.dcl.accommodate;
 
+import com.dcl.accommodate.security.jwt.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class AccommodationMarketplaceApplication {
 
 	public static void main(String[] args) {
-
         SpringApplication.run(AccommodationMarketplaceApplication.class, args);
 	}
-
 }
