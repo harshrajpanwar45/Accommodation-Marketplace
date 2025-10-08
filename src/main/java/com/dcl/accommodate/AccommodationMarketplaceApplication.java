d36fe45cfabd71579bf7da3ae1ee576dc04fe6e3
harshrajpanwar45 <harshrajpanwar45@gmail.com>
2025-10-08 12:39:05 +0530

    removed imports in main class

--- src/main/java/com/dcl/accommodate/AccommodationMarketplaceApplication.java
+++ src/main/java/com/dcl/accommodate/AccommodationMarketplaceApplication.java
@@ -1,12 +1,9 @@
 package com.dcl.accommodate;
 
-import com.dcl.accommodate.security.jwt.JwtService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.Map;
 
 @SpringBootApplication
 @EnableJpaAuditing
